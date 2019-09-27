0f99b69987e7d0f72e3a4bf3b8b7255fa10ea816
Maximiliano Osorio <maxiosorio@gmail.com>
2019-09-27 12:53:02 -0300

    Release v1.0.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="obasparql",
-    version="1.1.1",
+    version="1.0.0",
     description="OBA Sparql Manager",
     long_description=README,
     long_description_content_type="text/markdown",
