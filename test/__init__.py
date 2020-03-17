f2f2fa75568ec60347236a74ac71cbd0e0f1fb3c
Maximiliano Osorio <maxiosorio@gmail.com>
2020-03-17 12:51:50 -0700

    fix: logging config

--- test/__init__.py
+++ test/__init__.py
@@ -5,12 +5,17 @@ from pathlib import Path
 from obasparql import QueryManager
 from test.settings import QUERY_DIRECTORY, CONTEXT_DIRECTORY, QUERIES_TYPES
 
+logging_file = Path(__file__).parent.parent / "logging_config.ini"
+try:
+    logging.config.fileConfig(logging_file)
+except:
+    logging.error("Logging config file does not exist {}".format(logging_file))
+    exit(0)
+logger = logging.getLogger(__name__)
 
 class BaseTestCase(unittest.TestCase):
     def __init__(self):
-        logging_file = Path(__file__).parent.parent / "logging_config.ini"
-        logging.config.fileConfig(logging_file)
-
+        logger.error("Starting")
         self.query_manager = QueryManager(queries_dir=QUERY_DIRECTORY,
                                           context_dir=CONTEXT_DIRECTORY,
                                           queries_types=QUERIES_TYPES)
