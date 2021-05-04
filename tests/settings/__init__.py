679712261d57bec7eb7277b86d089ac83df3ea1a
Maximiliano Osorio <mosorio@isi.edu>
2021-05-04 07:20:41 -0700

    fix: add fuseki container

--- tests/settings/__init__.py
+++ tests/settings/__init__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 path = Path(__file__).parent.parent.parent
 
-model_catalog_endpoint = "https://endpoint.mint.isi.edu/modelCatalog-1.3.0"
+model_catalog_endpoint = "http://localhost:3030/modelCatalog-1.7.0"
 model_catalog_graph_base = "http://ontosoft.isi.edu:3030/modelCatalog-1.3.0/data/"
 model_catalog_prefix = "https://w3id.org/okn/i/mint/"
 model_catalog_queries = path / "tests/model_catalog/queries/"
