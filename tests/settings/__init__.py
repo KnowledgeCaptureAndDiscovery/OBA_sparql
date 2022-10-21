f9aede25bd6ab5c52bf8017360cd7a76ef13bb5c
Maximilinano Osorio <mosorio@isi.edu>
2022-10-21 09:55:56 -0300

    fix: update endpoints

--- tests/settings/__init__.py
+++ tests/settings/__init__.py
@@ -2,22 +2,24 @@ from pathlib import Path
 
 path = Path(__file__).parent.parent.parent
 
-model_catalog_endpoint = "https://endpoint.mint.isi.edu/modelCatalog-1.3.0"
-model_catalog_graph_base = "http://ontosoft.isi.edu:3030/modelCatalog-1.3.0/data/"
+model_catalog_endpoint = "https://endpoint.mint.isi.edu/dataset"
+model_catalog_graph_base = "http://endpoint.mint.isi.edu/modelCatalog-1.8.0/data/"
 model_catalog_prefix = "https://w3id.org/okn/i/mint/"
 model_catalog_queries = path / "tests/model_catalog/queries/"
 model_catalog_context = path / "tests/model_catalog/contexts/"
 
+# dev endpoint version
+model_catalog_endpoint_dev = model_catalog_endpoint 
+model_catalog_graph_base_dev = model_catalog_graph_base 
+model_catalog_prefix_dev = "https://w3id.org/okn/i/mint/"
+model_catalog_queries_dev = path / "tests/model_catalog_dev/queries/"
+model_catalog_context_dev = path / "tests/model_catalog_dev/contexts/"
+
+# dbpedia
 dbpedia_endpoint = "https://dbpedia.org/sparql"
 dbpedia_prefix = "http://dbpedia.org/resource"
 dbpedia_queries = path / "tests/dbpedia/queries/"
 dbpedia_context = path / "tests/dbpedia/contexts/"
 
-# dev endpoint version
-model_catalog_endpoint_dev = "https://endpoint.dev.mint.isi.edu/modelCatalog"
-model_catalog_graph_base_dev = "http://endpoint.mint.isi.edu/modelCatalog-1.8.0/data/"
-model_catalog_prefix_dev = "https://w3id.org/okn/i/mint/"
-model_catalog_queries_dev = path / "tests/model_catalog_dev/queries/"
-model_catalog_context_dev = path / "tests/model_catalog_dev/contexts/"
 
 
